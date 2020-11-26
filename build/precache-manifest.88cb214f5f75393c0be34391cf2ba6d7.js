0d0ba5b0094aa021554f20df95945b557bbab71e
Lucas <61996547+lucasfrontend@users.noreply.github.com>
2020-11-25 22:53:44 -0300

    Add files via upload
    Add build

---
+++ build/precache-manifest.88cb214f5f75393c0be34391cf2ba6d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a5bb585a63872d5792a88d4394f6d8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bce3a1ca04242410057",
+    "url": "/static/css/2.1afe660f.chunk.css"
+  },
+  {
+    "revision": "c7a403f75e181a9838fe",
+    "url": "/static/css/main.877eb9d7.chunk.css"
+  },
+  {
+    "revision": "4bce3a1ca04242410057",
+    "url": "/static/js/2.716f3d62.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/static/js/2.716f3d62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7a403f75e181a9838fe",
+    "url": "/static/js/main.038f1618.chunk.js"
+  },
+  {
+    "revision": "c42fd02281de16293b3c",
+    "url": "/static/js/runtime-main.63f96153.js"
+  }
+]);
